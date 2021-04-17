c646fe988a9d7ecd891269f327f6ce9fb346fab2
Isabelle Villasenor <villasenori@outlook.com>
2021-04-17 12:28:15 -0600

    Alter range on create_grid

--- lib/grid.rb
+++ lib/grid.rb
@@ -3,13 +3,13 @@ class Grid
   def initialize(grid_size)
     raise ArgumentError.new('Grid Size Must Be An Odd Number, Please Try Again') unless grid_size.odd?
     raise ArgumentError.new('Grid Size Must Be Greater Than 2, Please Try Again') unless grid_size > 2
-    
+
     @grid = Array.new(grid_size)
     create_grid(grid_size)
   end
 
   def create_grid(grid_size)
-    (0...grid_size).each do |i|
+    (3...grid_size).each do |i|
       grid[i] = '-' * grid_size
     end
   end
