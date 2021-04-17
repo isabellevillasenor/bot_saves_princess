efcec528316b54eddac9f644026a671692776b02
Isabelle Villasenor <villasenori@outlook.com>
2021-04-17 13:54:38 -0600

    Add Grid Place Bot Spec

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -14,4 +14,12 @@ describe Grid do
     expect{ Grid.new(4) }.to raise_error(ArgumentError)
     expect{ Grid.new(1) }.to raise_error(ArgumentError)
   end
+
+  it 'should instantiate with the bot directly in the middle' do
+    grid_1 = Grid.new(3)
+    grid_2 = Grid.new(5)
+
+    expect(grid_1.grid[1]).to include('-m-')
+    expect(grid_2.grid[2]).to include('-m-')
+  end
 end
