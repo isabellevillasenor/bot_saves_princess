0ee769a1d8a136797e1db4bafd6f534e47d393ee
Isabelle Villasenor <villasenori@outlook.com>
2021-04-17 17:48:19 -0600

    Add Princess Placement Method Spec

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -17,9 +17,19 @@ describe Grid do
 
   it 'should instantiate with the bot directly in the middle' do
     grid_1 = Grid.new(3)
-    grid_2 = Grid.new(5)
+    grid_2 = Grid.new(99)
 
     expect(grid_1.grid[1.5][1.5]).to eq('m')
-    expect(grid_2.grid[2.5][2.5]).to eq('m')
+    expect(grid_2.grid[49.5][49.5]).to eq('m')
+  end
+
+  it 'should instantiate with the princess in a corner' do
+    grid_1 = Grid.new(3)
+    grid_2 = Grid.new(99)
+
+    expect(grid_1.grid[0].include?('p') || grid_1.grid[2].include?('p')).to be true
+    expect(grid_1.grid[1].include?('p')).to be false
+    expect(grid_2.grid[0].include?('p') || grid_1.grid[98].include?('p')).to be true
+    expect(grid_2.grid[1...97].include?('p')).to be false
   end
 end
