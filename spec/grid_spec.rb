980e6a5e611ce556b1184ea30ea94c26c5856cfd
Isabelle Villasenor <villasenori@outlook.com>
2021-04-17 12:18:18 -0600

    Add Create Grid Spec

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,11 +1,18 @@
 require 'rspec'
 require 'pry'
-require_relative '../lib/grid.rb'
+require_relative '../lib/grid'
 
 describe Grid do
   it 'should instantiate with correct grid size' do
-    grid = Grid.new(15)
+    grid_1 = Grid.new(3)
+    grid_2 = Grid.new(99)
 
-    expect(grid.grid.size).to eq(15)
+    expect(grid_1.grid.size).to eq(3)
+    expect(grid_2.grid.size).to eq(99)
+  end
+
+  it 'should error when trying to instantiate with an even number of grid cells' do
+    expect{ Grid.new(4) }.to raise_error(ArgumentError)
+    expect{ Grid.new(1) }.to raise_error(ArgumentError)
   end
 end
