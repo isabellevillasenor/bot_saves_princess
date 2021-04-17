4466a5279fbe940d06e92c7f2eaf439c60f0ab75
Isabelle Villasenor <villasenori@outlook.com>
2021-04-17 12:18:46 -0600

    Add Create Grid Method with Initialize Error Handling

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,6 +1,9 @@
 class Grid
   attr_reader :grid
   def initialize(grid_size)
+    raise ArgumentError.new('Grid Size Must Be An Odd Number, Please Try Again') unless grid_size.odd?
+    raise ArgumentError.new('Grid Size Must Be Greater Than 2, Please Try Again') unless grid_size > 2
+    
     @grid = Array.new(grid_size)
     create_grid(grid_size)
   end
