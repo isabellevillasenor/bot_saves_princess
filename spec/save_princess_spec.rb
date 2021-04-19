c7ee681a958eb7e6533cb04d0225c76892e463e5
Isabelle Villasenor <villasenori@outlook.com>
2021-04-18 22:06:54 -0600

    Add Save Princess Spec

---
+++ spec/save_princess_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+require_relative '../lib/save_princess'
+require_relative '../lib/bot'
+require_relative '../lib/grid'
+
+describe SavePrincess do
+  it 'should instantiate with a new instance of grid and bot' do
+    round1 = SavePrincess.new(3)
+
+    expect(round1.bot).to be_a Bot
+    expect(round1.grid).to be_a Grid
+  end
+
+  it 'should return an array with the path to the princess' do
+    round1 = SavePrincess.new(3)
+
+    expect(round1.path_to_princess).to be_an Array
+  end
+end
