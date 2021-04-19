b725dbdedde0fa802f440b15a8eca65284d08c90
Isabelle Villasenor <villasenori@outlook.com>
2021-04-19 11:22:53 -0600

    Add Conditional Statments Specs for Save Princess Movement

--- spec/BSP2/save_princess_spec.rb
+++ spec/BSP2/save_princess_spec.rb
@@ -4,13 +4,13 @@ require_relative '../../BSP2_lib/grid'
 
 describe SavePrincess do
   it 'should instantiate with a new instance of grid' do
-    round1 = SavePrincess.new(3)
+    round1 = SavePrincess.new(3, 0, 0)
 
     expect(round1.grid).to be_a Grid
   end
 
   it 'should instantiate with attributes' do
-    round1 = SavePrincess.new(3)
+    round1 = SavePrincess.new(3, 0, 0)
 
     expect(round1.bot_column).to be_an Integer
     expect(round1.bot_row).to be_an Integer
@@ -18,9 +18,75 @@ describe SavePrincess do
     expect(round1.princess_row).to be_an Integer
   end
 
-  it 'should move to princess' do
-    round1 = SavePrincess.new(3)
+  it 'should move left horizontal when on same row' do
+    round1 = SavePrincess.new(3, 1, 2)
+
+    round1.princess_row = 1
+    round1.princess_column = 0
+    round1.next_move
 
     expect(round1.next_move).to be_a String
+    expect(round1.bot_column).to be_zero
+  end
+
+  it 'should move right horizontal when on same row' do
+    round1 = SavePrincess.new(3, 1, 1)
+
+    round1.princess_row = 1
+    round1.princess_column = 2
+    round1.next_move
+
+    expect(round1.bot_column).to eq(2)
+  end
+
+  it 'should move vertically down if not on same row' do
+    round1 = SavePrincess.new(3, 1, 2)
+
+    round1.princess_row = 2
+    round1.princess_column = 0
+
+    expect(round1.next_move).to be_a String
+    expect(round1.bot_row).to eq(2)
+  end
+
+  it 'should move vertically up if not on same row' do
+    round1 = SavePrincess.new(3, 1, 2)
+
+    round1.princess_row = 0
+    round1.princess_column = 0
+
+    expect(round1.next_move).to be_a String
+    expect(round1.bot_row).to eq(0)
+  end
+
+  it 'should move vertically up if in the same column' do
+    round1 = SavePrincess.new(3, 1, 2)
+
+    round1.princess_row = 0
+    round1.princess_column = 2
+
+    expect(round1.next_move).to be_a String
+    expect(round1.bot_row).to eq(0)
+  end
+
+  it 'should move vertically down if in the same column' do
+    round1 = SavePrincess.new(3, 1, 2)
+
+    round1.princess_row = 2
+    round1.princess_column = 2
+
+    expect(round1.next_move).to be_a String
+    expect(round1.bot_row).to eq(2)
+  end
+
+  it 'should know when princess is saved' do
+    round1 = SavePrincess.new(3, 0, 1)
+
+    round1.princess_row = 0
+    round1.princess_column = 0
+
+    expect(round1.next_move).to be_a String
+    expect(round1.bot_row).to be_zero
+    expect(round1.princess_saved).to be true
   end
 end
