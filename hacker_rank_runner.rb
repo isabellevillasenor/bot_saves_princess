1c6c504edf423df281d58dbf7920c76bd730c938
Isabelle Villasenor <villasenori@outlook.com>
2021-04-18 22:27:39 -0600

    Add Hacker Rank Runner File

--- hacker_rank_runner.rb
+++ hacker_rank_runner.rb
@@ -1,7 +1,106 @@
-# Here is where the compiled code with live once complete to copy/paste into HackerRank
+class Grid
+  attr_reader :grid, :princess, :dynamic_corner
+
+  def initialize(grid_size)
+    raise ArgumentError.new('Grid Size Must Be An Odd Number, Please Try Again') unless grid_size.odd?
+    raise ArgumentError.new('Grid Size Must Be Greater Than 2, Please Try Again') unless grid_size > 2
+
+    @dynamic_corner = grid_size - 1
+    @grid = Array.new(grid_size)
+    create_grid(grid_size)
+    place_bot
+    place_princess
+    @princess = princess_coordinates.compact.flatten
+  end
+
+  def create_grid(grid_size)
+    (0...grid_size).each do |i|
+      @grid[i] = '-' * grid_size
+    end
+  end
+
+  def place_bot
+    @grid[(@dynamic_corner / 2)][(@dynamic_corner / 2)] = 'm'
+  end
+
+  def place_princess
+    corner = 0, -1
+    @grid[corner.sample][corner.sample] = 'p'
+  end
+
+  def princess_coordinates
+    corners = [0, 0], [0, @dynamic_corner], [@dynamic_corner, 0], [@dynamic_corner, @dynamic_corner]
+    corners.map do |corner|
+      corner if @grid[corner[0]][corner[1]] == 'p'
+    end
+  end
+end
+
+class Bot < Grid
+  attr_reader :x,
+              :y,
+              :princess_x,
+              :princess_y
+
+  def initialize(grid)
+    @x = grid.grid.size / 2
+    @y = grid.grid.size / 2
+    @princess_x = grid.princess.first
+    @princess_y = grid.princess.last
+  end
+end
+
+class SavePrincess < Bot
+  attr_accessor :grid,
+                :x,
+                :y,
+                :princess_x,
+                :princess_y
+  attr_reader   :grid, :bot
+
+  def initialize(grid_size)
+    @grid = Grid.new(grid_size)
+    @bot = Bot.new(@grid)
+    @y = bot.y
+    @x = bot.x
+    @princess_y = bot.princess_y
+    @princess_x = bot.princess_x
+  end
+
+  def path_to_princess
+    moves_to_princess = []
+    moves_to_princess << vertical_movement until @x == @princess_x
+    moves_to_princess << horizontal_movement until @y == @princess_y
+    moves_to_princess
+  end
+
+  def horizontal_movement
+    if @y > @princess_y
+      @y -= 1
+      'LEFT'
+    elsif @y < @princess_y
+      @y += 1
+      'RIGHT'
+    end
+  end
+
+  def vertical_movement
+    if @x > @princess_x
+      @x -= 1
+      'UP'
+    elsif @x < @princess_x
+      @x += 1
+      'DOWN'
+    end
+  end
+end
 
 def displayPathtoPrincess(n,grid)
-  return ""
+  save_princess = SavePrincess.new(n)
+
+  save_princess.path_to_princess.each do |movement|
+    puts movement
+  end
 end
 
 m = gets.to_i
