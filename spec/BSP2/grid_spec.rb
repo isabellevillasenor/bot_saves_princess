1b2b8bf66a9f201e62a3f0fc038fdd61172b8b5f
Isabelle Villasenor <villasenori@outlook.com>
2021-04-18 23:58:34 -0600

    Fix Typo

--- spec/BSP2/grid_spec.rb
+++ spec/BSP2/grid_spec.rb
@@ -14,7 +14,7 @@ describe Grid do
     grid_1 = Grid.new(3)
 
     expect(grid_1.bot_row).to be_an Integer
-    expect(grid_1.bot_collumn).to be_an Integer
+    expect(grid_1.bot_column).to be_an Integer
     expect(grid_1.grid[0].include?('m') || grid_1.grid[1].include?('m') || grid_1.grid[2].include?('m')).to be true
   end
 
@@ -22,7 +22,7 @@ describe Grid do
     grid_1 = Grid.new(3)
 
     expect(grid_1.princess_row).to be_an Integer
-    expect(grid_1.princess_collumn).to be_an Integer
+    expect(grid_1.princess_column).to be_an Integer
     expect(grid_1.grid[0].include?('p') || grid_1.grid[1].include?('p') || grid_1.grid[2].include?('p')).to be true
   end
 end
