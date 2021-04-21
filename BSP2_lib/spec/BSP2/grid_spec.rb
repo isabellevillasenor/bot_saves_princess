23663776eaedf400e12ae1210f7226519f841558
Isabelle Villasenor <villasenori@outlook.com>
2021-04-21 09:28:58 -0600

    Refactor Grid Spec

--- BSP2_lib/spec/BSP2/grid_spec.rb
+++ BSP2_lib/spec/BSP2/grid_spec.rb
@@ -3,29 +3,16 @@ require_relative '../../grid'
 
 describe Grid do
   it 'should instantiate with correct grid size' do
-    grid_1 = Grid.new(3, 0, 0)
-    grid_2 = Grid.new(99, 0, 0)
+    grid_1 = Grid.new(["-----\n", "-----\n", "p--m-\n", "-----\n", "-----\n"])
 
-    expect(grid_1.grid.size).to eq(3)
-    expect(grid_2.grid.size).to eq(99)
-  end
-
-  it 'should instantiate with the bot placed from given arguments' do
-    grid_1 = Grid.new(3, 0, 0)
-
-    expect(grid_1.bot_r).to be_zero
-    expect(grid_1.bot_c).to be_zero
+    expect(grid_1.grid.size).to eq(5)
   end
 
   it 'should instantiate with the princess randomly placed' do
-    grid_1 = Grid.new(3, 0, 0)
+    grid_1 = Grid.new(["-----\n", "-----\n", "p--m-\n", "-----\n", "-----\n"])
 
-    expect(grid_1.princess_r).to be_an Integer
-    expect(grid_1.princess_c).to be_an Integer
-    expect(grid_1.grid[0].include?('p') || grid_1.grid[1].include?('p') || grid_1.grid[2].include?('p')).to be true
-  end
-
-  it 'should raise argument error if user tries to make board size less than 2' do
-    expect{ Grid.new(1) }.to raise_error(ArgumentError)
+    grid_1.princess_coordinates
+    expect(grid_1.princess_r).to eq(2)
+    expect(grid_1.princess_c).to eq(0)
   end
 end
