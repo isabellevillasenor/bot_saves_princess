e43fd05c52b568701f6c1239ff6f7df165813cd0
Isabelle Villasenor <villasenori@outlook.com>
2021-04-21 09:01:13 -0600

    Refactor HackerRank 2 Runner File

--- hacker_rank_2_runner.rb
+++ hacker_rank_2_runner.rb
@@ -1,93 +1,57 @@
+require 'pry'
 class Grid
   attr_reader :grid,
-              :bot_r,
-              :bot_c,
               :princess_r,
               :princess_c
 
-  def initialize(grid_size, row, column)
-    raise ArgumentError.new('Grid Size Must Be Greater Than 1, Please Try Again') unless grid_size > 1
-
-    @grid = Array.new(grid_size)
-    create_grid(grid_size)
-    @bot_r = nil
-    @bot_c = nil
-    place_bot(row, column)
+  def initialize(grid)
+    @grid = grid
     @princess_r = nil
     @princess_c = nil
-    place_princess(@bot_r, @bot_c, grid_size)
   end
 
-  def create_grid(grid_size)
-    (0...grid_size).each do |i|
-      @grid[i] = '-' * grid_size
-    end
-  end
+  def princess_coordinates
+    row = @grid.find_index { |r| r.include?('p') }
+    column = @grid[row].chars.find_index { |index| index == 'p' }
 
-  def place_bot(row, column)
-    @bot_r = row
-    @bot_c = column
-    @grid[@bot_r][@bot_c] = 'm'
-  end
-
-  def place_princess(bot_r, bot_c, grid_size)
-    @princess_r = rand(0...(grid_size - 1))
-    @princess_c = rand(0...(grid_size - 1))
-    if [@princess_r, @princess_c] != [bot_r, bot_c]
-      @grid[@princess_r][@princess_c] = 'p'
-    else
-      place_princess(bot_r, bot_c, grid_size)
-    end
+    @princess_r = row
+    @princess_c = column
   end
 end
 
-class Bot < Grid
-  attr_accessor :bot_row,
-                :bot_column,
-                :princess_row,
-                :princess_column
-
-  def initialize(grid)
-    @bot_row = grid.bot_r
-    @bot_column = grid.bot_c
-    @princess_row = grid.princess_r
-    @princess_column = grid.princess_c
-  end
-end
-
-class SavePrincess < Bot
+class SavePrincess
   attr_accessor :grid,
                 :bot_row,
                 :bot_column,
-                :princess_row,
-                :princess_column
+                :princess_r,
+                :princess_c
 
-  def initialize(grid_size, row, column)
-    @grid = Grid.new(grid_size, row, column)
-    @bot = Bot.new(@grid)
-    @bot_column = @bot.bot_column
-    @bot_row = @bot.bot_row
-    @princess_column = @bot.princess_column
-    @princess_row = @bot.princess_row
+  def initialize(n, row, column, grid)
+    @grid = Grid.new(grid)
+    @bot_column = column
+    @bot_row = row
+    @grid.princess_coordinates
+    @princess_column = @grid.princess_c
+    @princess_row = @grid.princess_r
+    @move = []
   end
 
   def next_move
-    move = nil
     if (@bot_row == @princess_row)
-      move = horizontal_movement
+      horizontal_movement
     else 
-      move = vertical_movement
+      vertical_movement
     end
-    move
+    puts @move
   end
 
   def horizontal_movement
     if @bot_column > @princess_column
       @bot_column -= 1
-      'LEFT'
+      @move << 'LEFT'
     elsif @bot_column < @princess_column
       @bot_column += 1
-      'RIGHT'
+      @move << 'RIGHT'
     end
   end
 
@@ -95,18 +59,18 @@ class SavePrincess < Bot
     if @bot_column == @princess_column
       if @bot_row > @princess_row
         @bot_row -= 1
-        'UP'
+        @move << 'UP'
       elsif @bot_row < @princess_row
         @bot_row += 1
-        'DOWN'
+        @move << 'DOWN'
       end
     else
       if @bot_row > @princess_row
         @bot_row -= 1
-        'UP'
+        @move << 'UP'
       elsif @bot_row < @princess_row
         @bot_row += 1
-        'DOWN'
+        @move << 'DOWN'
       end
     end
   end
@@ -117,8 +81,9 @@ class SavePrincess < Bot
 end
 
 
+
 def nextMove(n,r,c,grid)
-  save_princess = SavePrincess.new(n, r, c)
+  save_princess = SavePrincess.new(n, r, c, grid)
   save_princess.next_move
 end
 
@@ -129,7 +94,7 @@ r,c = gets.strip.split.map {|num| num.to_i}
 grid = Array.new(n)
 
 (0...n).each do |i|
-    grid[i] = gets
+  grid[i] = gets
 end
 
 nextMove(n,r,c,grid)
