6272ac89644e1fff62d08ee1a25a1c3365e58bbf
Isabelle Villasenor <villasenori@outlook.com>
2021-04-21 11:27:08 -0600

    Refactor SavePrincess Spec to Include Multiple Princess Placement Path Outputs

--- BSP1_lib/spec/BSP1/save_princess_spec.rb
+++ BSP1_lib/spec/BSP1/save_princess_spec.rb
@@ -5,15 +5,42 @@ require_relative '../../grid'
 
 describe SavePrincess do
   it 'should instantiate with a new instance of grid and bot' do
-    round1 = SavePrincess.new(3)
+    round1 = SavePrincess.new(3, ["---", "-m-", "p--"])
 
     expect(round1.bot).to be_a Bot
     expect(round1.grid).to be_a Grid
   end
 
-  it 'should return an array with the path to the princess' do
-    round1 = SavePrincess.new(3)
+  it 'should instantiate with attributes' do
+    round1 = SavePrincess.new(3, ["---", "-m-", "p--"])
 
-    expect(round1.path_to_princess).to be_an Array
+    expect(round1.bot_column).to eq(1)
+    expect(round1.bot_row).to eq(1)
+    expect(round1.princess_column).to eq(0)
+    expect(round1.princess_row).to eq(2)
+  end
+
+  it 'should return an array with the path to the princess' do
+    round1 = SavePrincess.new(3, ["---", "-m-", "p--"])
+
+    expect(round1.path_to_princess).to eq(["DOWN", "LEFT"])
+  end
+
+  it 'should return an array with the path to the princess' do
+    round1 = SavePrincess.new(3, ["---", "-m-", "--p"])
+
+    expect(round1.path_to_princess).to eq(["DOWN", "RIGHT"])
+  end
+
+  it 'should return an array with the path to the princess' do
+    round1 = SavePrincess.new(3, ["p--", "-m-", "---"])
+
+    expect(round1.path_to_princess).to eq(["UP", "LEFT"])
+  end
+
+  it 'should return an array with the path to the princess' do
+    round1 = SavePrincess.new(3, ["--p", "-m-", "---"])
+
+    expect(round1.path_to_princess).to eq(["UP", "RIGHT"])
   end
 end
